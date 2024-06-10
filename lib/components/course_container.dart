9f01522b38f84d2ca16e265c5ff56b8a9fa72e44
LI,CHI-YU <111233017+Lichyo@users.noreply.github.com>
2024-06-11 01:11:52 +0800

    update note and input page (#8)

--- lib/components/course_container.dart
+++ lib/components/course_container.dart
@@ -1,5 +1,6 @@
 import 'package:database_project/model/course.dart';
 import 'package:flutter/material.dart';
+import 'package:database_project/view/note_input_page.dart';
 
 class CourseContainer extends StatelessWidget {
   const CourseContainer({
@@ -11,16 +12,25 @@ class CourseContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(
-        course.name,
-        style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 30.0),
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => InputPage(course: course,),
+          ),
+        );
+      },
+      child: ListTile(
+        title: Text(
+          course.name,
+          style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 30.0),
+        ),
+        subtitle: Text(
+          course.time.toString(),
+          style: TextStyle(color: Colors.grey.shade400),
+        ),
+        isThreeLine: true,
       ),
-      subtitle: Text(
-        course.time.toString(),
-        style: TextStyle(color: Colors.grey.shade400),
-      ),
-      isThreeLine: true,
     );
   }
 }
