e29008a3125209fd1611735acec998ea43fbac18
Lichyo <lichyo003@gmail.com>
2024-06-10 23:25:52 +0800

    restore main.dart and database_service.dart

---
+++ lib/database_service.dart
@@ -0,0 +1,93 @@
+import 'package:postgres/postgres.dart';
+import 'model/course.dart';
+import 'model/student.dart';
+
+class DatabaseService {
+  static Connection? _conn;
+  static bool _isUserLogin = false;
+
+  static bool get isUserLogin => _isUserLogin;
+  static Student? student;
+
+  static Future<void> login({required int ID, required String password}) async {
+    final result = await _conn!.execute(
+        "SELECT st_name, email, department FROM STUDENT WHERE st_id = '$ID' AND password = '$password';");
+    student = Student(
+      ID: ID,
+      name: result[0][0] as String,
+      email: result[0][1] as String,
+      department: result[0][2] as String,
+    );
+    if (student != null) {
+      _isUserLogin = true;
+    }
+  }
+
+  static Future<List<Course>> getCourse() async {
+    List<Course> courses = [];
+    final result = await _conn!.execute(
+        "SELECT C.course_id, C.course_name, C.teacher, C.week, C.time, C.classroom FROM course C, have H "
+            "WHERE st_id = '${student!.ID}' AND H.course_id = C.course_id;");
+    for (var course in result) {
+      int ID = course[0] as int;
+      String name = course[1] as String;
+      String teacher = course[2] as String;
+      String week = course[3] as String;
+      String time = course[4] as String;
+      String classroom = course[5] as String;
+      courses.add(
+        Course(
+          ID: ID,
+          name: name,
+          teacher: teacher,
+          classroom: classroom,
+          time: time,
+          week: week,
+        ),
+      );
+    }
+    return courses;
+  }
+
+  static Future<void> ensuredDatabaseActivate() async {
+    _conn = await Connection.open(
+      Endpoint(
+        host: 'localhost',
+        database: 'database_project',
+        username: 'postgres',
+        password: 'xxxxxxxx',
+      ),
+      settings: const ConnectionSettings(sslMode: SslMode.disable),
+    );
+  }
+
+  static Future<void> writeCoursesIntoDatabase(
+      {required List<Course> courses}) async {
+    int courseID = 0;
+    for (Course course in courses) {
+      var results = await _conn!.execute(
+        "INSERT INTO course(course_name, teacher, week, time, classroom)"
+            "VALUES ('${course.name}', '${course.teacher}', '${course.week}', '${course.time}', '${course.classroom}')"
+            "ON CONFLICT DO NOTHING "
+            "RETURNING course_id;",
+      );
+      if (results.isEmpty) {
+        results = await _conn!.execute(
+          "SELECT course_id "
+              "FROM course "
+              "WHERE teacher = '${course.teacher}' AND "
+              "week = '${course.week}' AND "
+              "time = '${course.time}';",
+        );
+      }
+      courseID = results[0][0] as int;
+      _bindingCoursesWithUser(courseID: courseID);
+    }
+  }
+
+  static Future<void> _bindingCoursesWithUser({required int courseID}) async {
+    _conn!.execute('INSERT INTO have(st_id, course_id)'
+        'VALUES(${student!.ID}, $courseID) '
+        'ON CONFLICT DO NOTHING');
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'view/welcome_page.dart';
+import 'database_service.dart';
+import 'mock_data/course_mock.dart';
+
+void main() async {
+  await DatabaseService.ensuredDatabaseActivate();
+  runApp(const Assistant());
+}
+
+class Assistant extends StatelessWidget {
+  const Assistant({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: const LoginPage(),
+    );
+  }
+}
